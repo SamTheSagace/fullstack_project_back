03168affcaac962868a93aff5e595f3cbaee015e
MaximeOriot <maxime9278@gmail.com>
2026-03-27 12:15:40 +0100

    order items by position

--- poker/serializers.py
+++ poker/serializers.py
@@ -20,9 +20,13 @@ class SessionMemberSerializer(serializers.ModelSerializer):
         ordering = ['joined_at']
 
 class SessionSerializer(serializers.ModelSerializer):
-    items = ItemSerializer(many=True, read_only=True)
+    items = serializers.SerializerMethodField()
     members = SessionMemberSerializer(many=True, read_only=True)
 
     class Meta:
         model = Session
         fields = "__all__"
+
+    def get_items(self, obj):
+        items = obj.items.order_by('position')
+        return ItemSerializer(items, many=True).data
